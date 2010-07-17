1e59b2ba828bb6c78d0cb9104f2078355f3ff3bf
draylson <draylson@3b43cd18-b881-7823-851e-a1b76eca9be6>
2010-07-17 23:07:28 +0000

    Changes in /docs

--- workspace/ProgTest/doc/database/database-schema.sql
+++ workspace/ProgTest/doc/database/database-schema.sql
@@ -2,7 +2,7 @@ DROP DATABASE progtest;
 CREATE DATABASE progtest;
 USE progtest;
 
-CREATE TABLE user (
+CREATE TABLE User (
 	idCode INT NOT NULL AUTO_INCREMENT,
 	userName VARCHAR(50) NOT NULL UNIQUE,
 	name VARCHAR(100),
@@ -12,32 +12,32 @@ CREATE TABLE user (
 	CONSTRAINT PK_user PRIMARY KEY (idCode)
 );
 
-CREATE TABLE course (
+CREATE TABLE Course (
 	idCode INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(50),
 	startDate DATE,
 	endDate DATE,
 	instructor INT NOT NULL,
 	CONSTRAINT PK_course PRIMARY KEY (idCode),
-	CONSTRAINT FK_course_instructor FOREIGN KEY (instructor) REFERENCES user (idCode) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT FK_course_instructor FOREIGN KEY (instructor) REFERENCES User (idCode) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE student_course (
 	student INT NOT NULL,
 	course INT NOT NULL,
 	CONSTRAINT PK_student_course PRIMARY KEY (student, course),
-	CONSTRAINT FK_student_course_student FOREIGN KEY (student) REFERENCES user (idCode) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT FK_student_course_course FOREIGN KEY (course) REFERENCES course (idCode) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT FK_student_course_student FOREIGN KEY (student) REFERENCES User (idCode) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT FK_student_course_course FOREIGN KEY (course) REFERENCES Course (idCode) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE oracle (
+CREATE TABLE Oracle (
 	idCode INT NOT NULL AUTO_INCREMENT,
 	title VARCHAR(50),
 	description VARCHAR(500),
 	CONSTRAINT PK_oracle PRIMARY KEY (idCode)
 );
 
-CREATE TABLE assignment (
+CREATE TABLE Assignment (
 	course INT NOT NULL,
 	idCode INT NOT NULL,
 	title VARCHAR(50),
@@ -54,18 +54,18 @@ CREATE TABLE assignment (
 	weightAllUses INT,
 	weightAllPotUses INT,
 	CONSTRAINT PK_assignment PRIMARY KEY (course, idCode),
-	CONSTRAINT FK_assignment_course FOREIGN KEY (course) REFERENCES course (idCode) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT FK_assignment_course FOREIGN KEY (course) REFERENCES Course (idCode) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE evaluation (
+CREATE TABLE Evaluation (
 	student INT NOT NULL,
 	course INT NOT NULL,
 	assignment INT NOT NULL,
 	submissionDate DATETIME,
 	score DOUBLE,
 	CONSTRAINT PK_evaluation PRIMARY KEY (student, course, assignment),
-	CONSTRAINT FK_evaluation_student FOREIGN KEY (student) REFERENCES user (idCode) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT FK_evaluation_assignment FOREIGN KEY (course, assignment) REFERENCES assignment (course, idCode) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT FK_evaluation_student FOREIGN KEY (student) REFERENCES User (idCode) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT FK_evaluation_assignment FOREIGN KEY (course, assignment) REFERENCES Assignment (course, idCode) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 DELIMITER $$
@@ -73,10 +73,10 @@ DELIMITER $$
 CREATE TRIGGER `insert_student_course` AFTER INSERT ON `student_course` 
     FOR EACH ROW BEGIN
 	DECLARE i INT DEFAULT 0;
-	SELECT MAX(assignment.idCode) INTO i FROM assignment WHERE assignment.course = NEW.course;
+	SELECT MAX(Assignment.idCode) INTO i FROM Assignment WHERE Assignment.course = NEW.course;
 	WHILE i > 0 DO
-		IF (SELECT COUNT(*) FROM assignment WHERE assignment.course = NEW.course AND assignment.idCode = i) > 0 THEN
-			INSERT INTO evaluation(student, course, assignment, score) VALUES(NEW.student, NEW.course, i, 0);
+		IF (SELECT COUNT(*) FROM Assignment WHERE Assignment.course = NEW.course AND Assignment.idCode = i) > 0 THEN
+			INSERT INTO Evaluation(student, course, assignment, score) VALUES(NEW.student, NEW.course, i, 0);
 		END IF;
 		SET i = i - 1;
 	END WHILE;
@@ -85,22 +85,22 @@ END;$$
 CREATE TRIGGER `remove_student_course` BEFORE DELETE ON `student_course` 
     FOR EACH ROW BEGIN
 	DECLARE i INT DEFAULT 0;
-	SELECT MAX(assignment.idCode) INTO i FROM assignment WHERE assignment.course = OLD.course;
+	SELECT MAX(Assignment.idCode) INTO i FROM Assignment WHERE Assignment.course = OLD.course;
 	WHILE i > 0 DO
-		IF (SELECT COUNT(*) FROM assignment WHERE assignment.course = OLD.course AND assignment.idCode = i) > 0 THEN
-			DELETE FROM evaluation WHERE evaluation.student = OLD.student AND evaluation.course = OLD.course AND assignment = i;
+		IF (SELECT COUNT(*) FROM Assignment WHERE Assignment.course = OLD.course AND Assignment.idCode = i) > 0 THEN
+			DELETE FROM Evaluation WHERE Evaluation.student = OLD.student AND Evaluation.course = OLD.course AND Assignment = i;
 		END IF;
 		SET i = i - 1;
 	END WHILE;
 END;$$
 
-CREATE TRIGGER `insert_assignment` AFTER INSERT ON `assignment` 
+CREATE TRIGGER `insert_assignment` AFTER INSERT ON `Assignment` 
     FOR EACH ROW BEGIN
 	DECLARE i INT DEFAULT 0;
 	SELECT MAX(student_course.student) INTO i FROM student_course WHERE student_course.course = NEW.course;
 	WHILE i > 0 DO
 		IF (SELECT COUNT(*) FROM student_course WHERE student_course.course = NEW.course AND student_course.student = i) > 0 THEN
-			INSERT INTO evaluation(student, course, assignment, score) VALUES(i, NEW.course, NEW.idCode, 0);
+			INSERT INTO Evaluation(student, course, assignment, score) VALUES(i, NEW.course, NEW.idCode, 0);
 		END IF;
 		SET i = i - 1;
 	END WHILE;
@@ -108,44 +108,44 @@ END;$$
 
 DELIMITER ;
 
-INSERT INTO user VALUES(1, 'progtest', 'ProgTest', 'progtest@gmail.com', 'progtest', 'instructor');
+INSERT INTO User VALUES(1, 'progtest', 'ProgTest', 'progtest@gmail.com', 'progtest', 'instructor');
 
-INSERT INTO oracle VALUES(1, 'Valor Máximo', 'Implementar um programa que obtem o maior valor de uma sequência de números. O programa deverá ter uma classe Main com um metodo main que recebe como parâmetro uma String representando uma sequência de números separados por vírgula e retorne uma String representando o maior número da sequência de entrada.');
+INSERT INTO Oracle VALUES(1, 'Maximum Value', 'Obtains the highest value of a sequence of integers.');
 
-INSERT INTO oracle VALUES(2, 'Valores Máximo e Mínimo', 'Implementar um programa que obtem o maior e o menor valor de uma sequência de números. O programa deverá ter uma classe Main com um metodo main que recebe como parâmetro uma String representando uma sequência de números separados por vírgula e retorne uma String representando o maior e menor número da sequência de entrada separados por vírgula.');
+INSERT INTO Oracle VALUES(2, 'Maximum and Minimum Value', 'Obtains the highest and lowest values of a sequence of integers.');
 
-INSERT INTO oracle VALUES(3, 'Fibonacci', 'Implementar um programa que calcula a equação de Fibonacci para um determinado número. O programa deverá ter uma classe Main com um metodo main que recebe como parâmetro uma String representando o número a ser calculado e retorna uma String representando o número resultante do cálculo.');
+INSERT INTO Oracle VALUES(3, 'Fibonacci', 'Computes the Fibonacci sequence.');
 
-INSERT INTO oracle VALUES(4, 'Casamento de Cadeias de Caracteres', 'Implementar um programa que realiza busca em cadeias de caracteres. O programa deverá ter uma classe Main com um metodo main que recebe como parâmetro duas Strings e retorna uma String representando o posição em que o valor das Strings de entrada casam.');
+INSERT INTO Oracle VALUES(4, 'String Marriage', 'Searches the occurrence of one string in another string.');
 
-INSERT INTO oracle VALUES(5, 'Ordenação', 'Implementar um programa que ordena uma sequência de números. O programa deverá ter uma classe Main com um metodo main que recebe como parâmetro uma String representando uma sequência de números separados por vírgula e retorne uma String representando a sequência de números ordenada.');
+INSERT INTO Oracle VALUES(5, 'Sort', 'Sorts a sequence of numbers.');
 
-INSERT INTO oracle VALUES(6, 'BubbleSort', 'Implementar o BubbleSrot. O programa deverá ter uma classe Main com um metodo main que recebe como parâmetro uma String representando uma sequência de números separados por vírgula e uma String representando o número de iterações do algorítmo deverá ser executada. O método deverá retornar uma String com a sequência de número obtida após realizar as iterações.');
+INSERT INTO Oracle VALUES(6, 'Bubble Sort', 'Implementation of the bubble sort algorithm.');
 
-INSERT INTO oracle VALUES(7, 'HeapSort', 'Implementar o HeapSrot. O programa deverá ter uma classe Main com um metodo main que recebe como parâmetro uma String representando uma sequência de números separados por vírgula e uma String representando o número de iterações do algorítmo deverá ser executada. O método deverá retornar uma String com a sequência de número obtida após realizar as iterações.');
+INSERT INTO Oracle VALUES(7, 'Heapsort', 'Implementation of the heapsort algorithm.');
 
-INSERT INTO oracle VALUES(8, 'Inserção Simples', 'Implementar o algorítmo de ordenação Inserção Simples. O programa deverá ter uma classe Main com um metodo main que recebe como parâmetro uma String representando uma sequência de números separados por vírgula e uma String representando o número de iterações do algorítmo deverá ser executada. O método deverá retornar uma String com a sequência de número obtida após realizar as iterações.');
+INSERT INTO Oracle VALUES(8, 'Insertion Sort', 'Implementation of the insertion sort algorithm.');
 
-INSERT INTO oracle VALUES(9, 'Seleção Simples', 'Implementar o algorítmo de ordenação Seleção Simples. O programa deverá ter uma classe Main com um metodo main que recebe como parâmetro uma String representando uma sequência de números separados por vírgula e uma String representando o número de iterações do algorítmo deverá ser executada. O método deverá retornar uma String com a sequência de número obtida após realizar as iterações.');
+INSERT INTO Oracle VALUES(9, 'Selection Sort', 'Implementation of the selection sort algorithm.');
 
-INSERT INTO oracle VALUES(10, 'Lista', 'Implementar uma lista. O programa deverá ter uma classe Lista com os métodos add, remove, print. O último deve retornar uma String com os valores presentes na lista separados por vírgula.');
+INSERT INTO Oracle VALUES(10, 'List', 'Implementation of a list.');
 
-INSERT INTO oracle VALUES(11, 'Pilha', 'Implementar uma pilha. O programa deverá ter uma classe Pilha com os métodos push, pop, print. O último deve retornar uma String com os valores presentes na pilha separados por vírgula.');
+INSERT INTO Oracle VALUES(11, 'Stack', 'Implementation of a stack.');
 
-INSERT INTO oracle VALUES(12, 'Fila', 'Implementar uma fila. O programa deverá ter uma classe Fila com os métodos add, remove, print. O último deve retornar uma String com os valores presentes na fila separados por vírgula.');
+INSERT INTO Oracle VALUES(12, 'Queue', 'Implementation of a queue.');
 
-INSERT INTO oracle VALUES(13, 'Árvore Binária', 'Implementar uma árvore binária. O programa deverá ter uma classe BTree com os métodos add, remove, print. O último deve retornar uma String com os valores presentes na árvore separados por vírgula.');
+INSERT INTO Oracle VALUES(13, 'Binary Tree', 'Implementation of a binary tree.');
 
-INSERT INTO oracle VALUES(14, 'Grafo', 'Implementar um grafo. O programa deverá ter uma classe Grafo com os métodos addEdge, removeEdge, print. O último deve retornar uma String com as arestas do Grafo.');
+INSERT INTO Oracle VALUES(14, 'Graph', 'Implementation of a graph.');
 
-INSERT INTO oracle VALUES(15, 'Busca em Profundidade', 'Implementar a busca em profundidade em um grafo. O programa deverá ter uma classe Grafo com os métodos add, remove, search. O último deve retornar uma String com os valores dos vertices na ordem que foram visitados.');
+INSERT INTO Oracle VALUES(15, 'Depth-first Search', 'Implementation of the depth-first search in a graph.');
 
-INSERT INTO oracle VALUES(16, 'Busca em Largura', 'Implementar a busca em largura em um grafo. O programa deverá ter uma classe Grafo com os métodos add, remove, search. O último deve retornar uma String com os valores dos vertices na ordem que foram visitados.');
+INSERT INTO Oracle VALUES(16, 'Breadth-first Search', 'Implementation of the breadth-first search in a graph.');
 
-INSERT INTO oracle VALUES(17, 'Componentes Fortemente Conectadas', 'Implementar um algorítimo para identificar as componentes fortemente conectadas de um Grafo. O programa deverá ter uma classe Grafo com os métodos add, remove e cfc. O último deve retornar uma String com o número de componentes fortemente conectadas.');
+INSERT INTO Oracle VALUES(17, 'Strongly Connected Components', 'Gets the number of strongly connected components of a graph.');
 
-INSERT INTO oracle VALUES(18, 'Algorítmo de Prim', 'Implementar o algorítmo de Prim. O programa deverá ter uma classe Grafo com os métodos add, remove e prim. O último deve retornar um Grafo correspondente a árvore geradora mínima');
+INSERT INTO Oracle VALUES(18, 'Prim Algorithm', 'Implementation of the Prim algorithm.');
 
-INSERT INTO oracle VALUES(19, 'Tabela Hash', 'Implementar uma tabela hash. O programa deverá ter uma classe Hash com os métodos add, remove, print. O último deve retornar uma String com os valores presentes na tabela hash separados por vírgula.');
+INSERT INTO Oracle VALUES(19, 'Hash Table', 'Implementation of a hash table.');
 
-INSERT INTO oracle VALUES(20, 'Avaliação de Multiplicação de Matrizes', 'Avaliação de Multiplicação de Matrizes');
+INSERT INTO Oracle VALUES(20, 'Matrix Multiplication', 'Gets the result of multiplying two matrices.');
